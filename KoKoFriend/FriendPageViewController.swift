65355de7f9571cbf5706dba3f7dbb5adba13c7b5
VictorHuang <victorandprograme@gmail.com>
2023-11-23 03:28:25 +0800

    [Feature] set FriendPageViewController UI

--- KoKoFriend/FriendPageViewController.swift
+++ KoKoFriend/FriendPageViewController.swift
@@ -8,22 +8,71 @@
 import UIKit
 
 class FriendPageViewController: UIViewController {
-
+    
+    @IBOutlet weak var segmentView: CustomSegmentedView!
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let withdrawNaviBtn = UIBarButtonItem(image:UIImage(named: "icNavPinkWithdraw") , style: .plain, target: self, action: nil)
+        let transNaviBtn = UIBarButtonItem(image:UIImage(named: "icNavPinkTransfer") , style: .plain, target: self, action: nil)
+        let scanNaviBtn = UIBarButtonItem(image:UIImage(named: "icNavPinkScan") , style: .plain, target: self, action: nil)
+        self.navigationItem.leftBarButtonItems = [withdrawNaviBtn,transNaviBtn]
+        self.navigationItem.rightBarButtonItems = [scanNaviBtn]
+       
+        
+        segmentView.delegate = self
+        segmentView.dataSource = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "EmptyFriendCell", bundle: nil), forCellReuseIdentifier: "EmptyFriendCell")
+        tableView.register(UINib(nibName: "SearchCell", bundle: nil), forCellReuseIdentifier: "SearchCell")
+        tableView.register(UINib(nibName: "FriendInfoCell", bundle: nil), forCellReuseIdentifier: "FriendInfoCell")
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
+        tableView.separatorStyle = .none
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+}
+
+extension FriendPageViewController: CustemSegmontedViewDelegate ,CustemSegmontedViewDataSource{
+    func didSelect(row: Int) -> Bool {
+        return true
     }
-    */
+    
+    func itemNumber() -> Int {
+        return 2
+    }
+    
+    func itemTitle(row: Int) -> String {
+        let titleArr = ["朋友","聊天"]
+        return titleArr[row]
+    }
+}
 
+
+extension FriendPageViewController:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath) as! SearchCell
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "FriendInfoCell", for: indexPath) as! FriendInfoCell
+            cell.setUpCellStatus(isTop: true, status: 1)
+            return cell
+        }
+       
+    }
+    
+    
 }
