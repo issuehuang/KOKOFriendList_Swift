db4c69ee0d379c114c897bddd77b1e6f3e56e6ac
VictorHuang <victorandprograme@gmail.com>
2023-11-22 14:05:40 +0800

    [Feature] add EmptyFriendCell and xib

---
+++ KoKoFriend/EmptyFriendCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmptyFriendCell.swift
+//  KoKoFriend
+//
+//  Created by Victor on 2023/11/22.
+//
+
+import UIKit
+
+class EmptyFriendCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
