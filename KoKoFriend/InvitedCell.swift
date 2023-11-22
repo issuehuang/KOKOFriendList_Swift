6dbc332bb272a1eb369c55b6a43bd53e7c9a6fbb
VictorHuang <victorandprograme@gmail.com>
2023-11-23 03:24:35 +0800

    [Feature] add icons & InviteCell

---
+++ KoKoFriend/InvitedCell.swift
@@ -0,0 +1,23 @@
+//
+//  InvitedCell.swift
+//  KoKoFriend
+//
+//  Created by Victor on 2023/11/23.
+//
+
+import UIKit
+
+class InvitedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
