21d1ae96119f0649be7b0868428c3302c12a45ac
VictorHuang <victorandprograme@gmail.com>
2023-11-23 03:27:21 +0800

    [Feature] set InvitedCell UI

--- KoKoFriend/InvitedCell.swift
+++ KoKoFriend/InvitedCell.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class InvitedCell: UITableViewCell {
 
+    @IBOutlet weak var avatorImgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var confirmBtn: UIButton!
+    @IBOutlet weak var deleteBtn: UIButton!
+    @IBOutlet weak var bgView: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        avatorImgView.image = UIImage(named: "imgFriendsFemaleDefault")
+        avatorImgView.layer.cornerRadius = 20
+        nameLabel.textColor = .greyishBrown
+        nameLabel.font = UIFont(name: "PingFangTC-Regular", size: 16)
+        greetingLabel.textColor = .brownGrey
+        greetingLabel.font =  UIFont(name: "PingFangTC-Regular", size: 13)
+        greetingLabel.text = "邀請你成為好友：）"
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
