9085dab87ef18cb14c75a4ee622e4c8c7a4f49f9
VictorHuang <victorandprograme@gmail.com>
2023-11-23 03:26:31 +0800

    [Feature] set SearchCell UI

--- KoKoFriend/SearchCell.swift
+++ KoKoFriend/SearchCell.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class SearchCell: UITableViewCell {
 
+    @IBOutlet weak var textfield: SearchTextField!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        textfield.leftViewMode = .always
+        textfield.leftView = UIImageView(image: UIImage(named: "icSearchBarSearchGray"))
+        
+        textfield.backgroundColor = UIColor(red: 142/255, green: 142/255, blue: 142/255, alpha: 0.12)
+        textfield.placeholder = "想轉一筆給誰呢？"
+        textfield.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,4 +26,19 @@ class SearchCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+
+class SearchTextField: UITextField {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        leftViewMode = .always
+        let imageView = UIImageView(image: UIImage(named: "icSearchBarSearchGray"))
+        imageView.contentMode = .scaleAspectFit
+        leftView = imageView
+    }
+    
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        let width = UIImage(named: "icSearchBarSearchGray")?.size.width ?? 0.0
+        return CGRect(x: 10, y: 10, width: width, height: 14)
+    }
 }
