af7d1c5fd65b545d106e22f7a6cfc5ea51719df1
VictorHuang <victorandprograme@gmail.com>
2023-11-24 03:29:35 +0800

    [Feature] ViewModel add buttons

--- KoKoFriend/ViewController.swift
+++ KoKoFriend/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+enum dataType {
+    case noFriend
+    case friendButNoInvite
+    case friendAndInvite
+}
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var noFriendsBtn: GradientButton!
+    
+    @IBOutlet weak var friendButNoInviteBtn: GradientButton!
+    
+    @IBOutlet weak var friendAndInviteBtn: GradientButton!
+    
+    lazy var friendBtns:[GradientButton] = {
+        return [noFriendsBtn,friendButNoInviteBtn,friendAndInviteBtn]
+    }()
+    var viewModel = FriendListViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        friendBtns.forEach { btn in
+            btn.startPoint = CGPoint(x: 0, y: 0.5)
+            btn.endPoint = CGPoint(x: 1, y: 0.5)
+            btn.colors = [UIColor.frogGreen.cgColor,UIColor.booger.cgColor]
+            btn.titleLabel?.textColor = .white
+            btn.tintColor = .white
+        }
+        noFriendsBtn.addTarget(self, action: #selector(tapNoFriendBtn), for: .touchUpInside)
+        friendButNoInviteBtn.addTarget(self, action: #selector(tapFriendButNoInviteBtn), for: .touchUpInside)
+        friendAndInviteBtn.addTarget(self, action: #selector(tapFriendAndInviteBtn), for: .touchUpInside)
+
+
     }
 
+    @objc func tapNoFriendBtn(){
+        UserManager.shared.apiType = .noFriend
+        self.tabBarController?.selectedIndex = 1
+    }
+    @objc func tapFriendButNoInviteBtn(){
+        UserManager.shared.apiType = .friendButNoInvite
+        viewModel.fetchDataType = .friendButNoInvite
 
+        self.tabBarController?.selectedIndex = 1
+       
+    }
+    @objc func tapFriendAndInviteBtn(){
+        UserManager.shared.apiType = .friendAndInvite
+        viewModel.fetchDataType = .friendAndInvite
+        self.tabBarController?.selectedIndex = 1
+    }
 }
 
