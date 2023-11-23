9e774c80fdecb70e5346fb99ef480edf6cafb3c2
VictorHuang <victorandprograme@gmail.com>
2023-11-24 03:30:41 +0800

    [Feature] FriendPageViewController add functions

--- KoKoFriend/FriendPageViewController.swift
+++ KoKoFriend/FriendPageViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 class FriendPageViewController: UIViewController {
     
     @IBOutlet weak var segmentView: CustomSegmentedView!
-    
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var inviteListTableView: UITableView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var kokoIDBtn: UIButton!
+    @IBOutlet weak var invitedFriendViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var headerViewConstraint: NSLayoutConstraint!
+    private var invitedfriendList:[FriendList]?
+    private var friendsList:[FriendList]?
+    private var viewModel = FriendListViewModel()
+    private var searchText:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         let withdrawNaviBtn = UIBarButtonItem(image:UIImage(named: "icNavPinkWithdraw") , style: .plain, target: self, action: nil)
@@ -19,7 +27,7 @@ class FriendPageViewController: UIViewController {
         let scanNaviBtn = UIBarButtonItem(image:UIImage(named: "icNavPinkScan") , style: .plain, target: self, action: nil)
         self.navigationItem.leftBarButtonItems = [withdrawNaviBtn,transNaviBtn]
         self.navigationItem.rightBarButtonItems = [scanNaviBtn]
-       
+        
         
         segmentView.delegate = self
         segmentView.dataSource = self
@@ -31,11 +39,39 @@ class FriendPageViewController: UIViewController {
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 100
         tableView.separatorStyle = .none
+        
+        inviteListTableView.delegate = self
+        inviteListTableView.dataSource = self
+        inviteListTableView.register(UINib(nibName: "InvitedCell", bundle: nil), forCellReuseIdentifier: "InvitedCell")
+        inviteListTableView.separatorStyle = .none
+        
+        
+       
+     
+        viewModel.reloadViewClosure = { [weak self] in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+                self?.inviteListTableView.reloadData()
+            }
+        }
+    }
+        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.userNameLabel.text = viewModel.user?.first?.name
+        let kokoid = viewModel.user?.first?.kokoid ?? ""
+        self.kokoIDBtn.setTitle("KOKO ID : \(kokoid)", for: .normal)
+        viewModel.fetchData()
+        if UserManager.shared.apiType == .friendAndInvite {
+            invitedFriendViewHeightConstraint.constant = 175
+            headerViewConstraint.constant = 268
+        }else {
+            invitedFriendViewHeightConstraint.constant = 0
+            headerViewConstraint.constant = 268 - 175
+        }
     }
     
-
-
-
+    
 }
 
 extension FriendPageViewController: CustemSegmontedViewDelegate ,CustemSegmontedViewDataSource{
@@ -56,23 +92,63 @@ extension FriendPageViewController: CustemSegmontedViewDelegate ,CustemSegmonted
 
 extension FriendPageViewController:UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        switch tableView {
+        case inviteListTableView:
+            return viewModel.invitedfriendList?.count ?? 0
+        case tableView:
+            return ((viewModel.friendsList?.count ?? 0) + 1)
+        default:
+            return 2
+        }
+        
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
+        if tableView == inviteListTableView {
+            return 70
+        }else{
+            if viewModel.friendsList?.count != 0 {
+                return 60
+            }else{
+                return UITableView.automaticDimension
+            }
+                
+           
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath) as! SearchCell
+        switch tableView {
+        case inviteListTableView:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "InvitedCell", for: indexPath) as! InvitedCell
+            cell.nameLabel.text = viewModel.invitedfriendList?[indexPath.row].name
             return cell
-        }else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "FriendInfoCell", for: indexPath) as! FriendInfoCell
-            cell.setUpCellStatus(isTop: true, status: 1)
-            return cell
+        case tableView:
+            if indexPath.row == 0 {
+                if viewModel.friendsList?.count == 0 {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "EmptyFriendCell", for: indexPath) as! EmptyFriendCell
+                    return cell
+                }else {
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath) as! SearchCell
+                    cell.searchBtn.addTarget(self, action: #selector(tapSearchBtn), for: .touchUpInside)
+                    return cell
+                }
+            }else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: "FriendInfoCell", for: indexPath) as! FriendInfoCell
+                guard let friend = viewModel.friendsList?[indexPath.row - 1] else { return  UITableViewCell()}
+                cell.setUpCellStatus(isTop: friend.isTop, status: friend.status)
+                cell.nameLabel.text = friend.name
+                return cell
+            }
+        default:
+            return UITableViewCell()
         }
-       
+        
+        
     }
     
-    
+    @objc func tapSearchBtn(){
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as? SearchCell
+        viewModel.searchFriend(text:cell?.textfield.text ?? "")
+    }
 }
